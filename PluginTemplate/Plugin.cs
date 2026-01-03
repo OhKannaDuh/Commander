ae6f4297402b68c916ced5b4d661a84cc73a2a0c
Faye <Liammcnair@outlook.com>
2026-01-03 17:54:16 +0000

    Initial commit

---
+++ PluginTemplate/Plugin.cs
@@ -0,0 +1,35 @@
+﻿using Dalamud.Plugin;
+using Microsoft.Extensions.DependencyInjection;
+using Ocelot;
+// using Ocelot.Chain.Services;
+// using Ocelot.ECommons.Services;
+// using Ocelot.Mechanic.Services;
+// using Ocelot.Pathfinding.Services;
+// using Ocelot.Pictomancy.Services;
+// using Ocelot.Rotation.Services;
+// using Ocelot.UI.Services;
+
+namespace PluginTemplate;
+
+public sealed class Plugin(IDalamudPluginInterface plugin) : OcelotPlugin(plugin)
+{
+    private readonly IDalamudPluginInterface plugin = plugin;
+
+    public override string Name { get; } = "PluginTemplate";
+
+    protected override void Boostrap(IServiceCollection services)
+    {
+        BootstrapOcelotModules(services);
+    }
+
+    private static void BootstrapOcelotModules(IServiceCollection services)
+    {
+        // services.LoadECommons();
+        // services.LoadPictomancy();
+        // services.LoadPathfinding();
+        // services.LoadMechanics();
+        // services.LoadRotations();
+        // services.LoadChain();
+        // services.LoadUI();
+    }
+}
